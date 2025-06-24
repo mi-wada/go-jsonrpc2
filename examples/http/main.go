4c339edd56a1b3f0ce437349498a82fdf98d8ae1
mi-wada <mtak.wd@gmail.com>
2025-06-24 18:59:35 +0900

    fix examples

--- examples/http/main.go
+++ examples/http/main.go
@@ -3,21 +3,63 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/mi-wada/go-jsonrpc2"
 )
 
-func main() {
+func runServer() {
 	// Create and configure the HTTP server
 	server := jsonrpc2.NewHTTPServer(":8080", "/rpc")
 	server.Register("add", addHandler)
 	server.Register("subtract", subtractHandler)
 
+	fmt.Println("JSON-RPC 2.0 HTTP server starting on :8080")
+
 	// Run the server
 	ctx := context.Background()
 	if err := server.Run(ctx); err != nil {
 		log.Printf("Server error: %v", err)
+	}
+}
+
+func runClient(data string) {
+	client := jsonrpc2.NewHTTPClient("http://localhost:8080/rpc", nil)
+
+	req, err := jsonrpc2.UnmarshalRequest([]byte(data))
+	if err != nil {
+		log.Fatal("Error parsing request data:", err)
+	}
+
+	resp, err := client.Call(context.Background(), req)
+	if err != nil {
+		log.Fatal("Error calling RPC:", err)
+	}
+
+	respData, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatal("Error marshaling response:", err)
+	}
+
+	fmt.Println(string(respData))
+}
+
+func main() {
+	var mode = flag.String("m", "server", "Mode: server or client")
+	var data = flag.String("d", "", "JSON-RPC request data (for client mode)")
+	flag.Parse()
+
+	if *mode == "client" {
+		if *data == "" {
+			fmt.Println("Usage: go run main.go -m client -d '<json_data>'")
+			os.Exit(1)
+		}
+		runClient(*data)
+	} else {
+		runServer()
 	}
 }
 
--- examples/tcp/main.go
+++ examples/tcp/main.go
@@ -3,21 +3,70 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
+	"net"
+	"os"
 
 	"github.com/mi-wada/go-jsonrpc2"
 )
 
-func main() {
+func runServer() {
 	// Create and configure the TCP server
 	server := jsonrpc2.NewTCPServer(":8081")
 	server.Register("add", addHandler)
 	server.Register("subtract", subtractHandler)
 
+	fmt.Println("JSON-RPC 2.0 TCP server starting on :8081")
+
 	// Run the server
 	ctx := context.Background()
 	if err := server.Run(ctx); err != nil {
 		log.Printf("Server error: %v", err)
+	}
+}
+
+func runClient(data string) {
+	conn, err := net.Dial("tcp", "localhost:8081")
+	if err != nil {
+		log.Fatal("Error connecting to server:", err)
+	}
+	defer conn.Close()
+
+	client := jsonrpc2.NewTCPClient(conn)
+
+	req, err := jsonrpc2.UnmarshalRequest([]byte(data))
+	if err != nil {
+		log.Fatal("Error parsing request data:", err)
+	}
+
+	resp, err := client.Call(context.Background(), req)
+	if err != nil {
+		log.Fatal("Error calling RPC:", err)
+	}
+
+	respData, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatal("Error marshaling response:", err)
+	}
+
+	fmt.Println(string(respData))
+}
+
+func main() {
+	var mode = flag.String("m", "server", "Mode: server or client")
+	var data = flag.String("d", "", "JSON-RPC request data (for client mode)")
+	flag.Parse()
+
+	if *mode == "client" {
+		if *data == "" {
+			fmt.Println("Usage: go run main.go -m client -d '<json_data>'")
+			os.Exit(1)
+		}
+		runClient(*data)
+	} else {
+		runServer()
 	}
 }
 
